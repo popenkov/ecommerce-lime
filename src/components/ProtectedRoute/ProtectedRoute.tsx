4d3921e19d28b8d41cbfdf836ed8c5afbd736fc7
Anton Popenkov <popenkovaa@yandex.ru>
2023-01-30 10:52:59 +0300

    auth added

---
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import { useActions } from "@src/hooks/useActions";
+import { FC, ReactElement, ReactNode } from "react";
+import { Navigate } from "react-router-dom/dist";
+
+import { useAuth } from "../../hooks/useAuth";
+
+type ProtectedRouteType = {
+  children?: ReactNode;
+};
+
+export const ProtectedRoute: FC<ProtectedRouteType> = ({ children }) => {
+  const { user } = useAuth();
+  const { openAuthPopup } = useActions();
+
+  if (!user) {
+    openAuthPopup();
+    return <Navigate to="/" replace />;
+  }
+  if (children) return children;
+};
